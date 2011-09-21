8557797fc5baf0f204365362c8bef332639da363
Romaric Drigon <romaric.drigon@gmail.com>
2011-09-21 10:36:07 +0200

    V1.0
    First version, based on Code Igniter framework
    Basic functions only : add a section, change an item

---
+++ application/controllers/request.php
@@ -0,0 +1,116 @@
+<?php
+class Request extends CI_Controller {
+	/* 
+	 * Classe qui fait tout le boulot actuellement
+	 */
+	
+	function index()
+	{
+		die('Direct access forbidden');
+	}
+	
+	/*
+	 * Recoit un mot (fixé) codé avec le mot de passe,
+	 * compare avec la bdd
+	 * si c'est conforme renvoie ok
+	 * @param, en POST :
+	 *  - id de l'user
+	 *  - password ("key")
+	 */
+	public function gets()
+	{
+		$user = $this->input->post('user');
+		$key = $this->input->post('key');
+		
+		if (strlen($user) == 0 || strlen($key) == 0)
+		{
+			die("Missing parameter");
+		}
+		
+		// oui, en Php il faut vraiment faire ça, avec strict à TRUE, pour tester si c'est bien du base64 !
+		if (base64_decode($key, TRUE) === FALSE)
+		{
+			die("Wrong encoding");
+		}
+		
+		$this->load->model('request_model');
+		
+		if ($this->request_model->valid_credentials($user, $key) === TRUE)
+		{
+			// get all
+			$data = $this->request_model->get_blops();
+			
+			echo json_encode($data);
+		}
+		else
+		{
+			die("Invalid credentials");
+		}
+	}
+	
+	/*
+	 * Ajouter une section
+	 */
+	public function add_section()
+	{
+		$title = $this->input->post('title');
+		$user = $this->input->post('user');
+		$key = $this->input->post('key');
+		
+		if (strlen($title) == 0 || strlen($user) == 0 || strlen($key) == 0)
+		{
+			die("Missing parameter");
+		}
+		
+		if (base64_decode($title, TRUE) === FALSE)
+		{
+			die("Wrong encoding");
+		}
+		
+		$this->load->model('request_model');
+		
+		if ($this->request_model->valid_credentials($user, $key) === TRUE)
+		{
+			echo $this->request_model->add_section($title);
+		}
+		else
+		{
+			die("Invalid credentials");
+		}
+	}
+	
+	/*
+	 * Modifier un item, on ne sait pas quoi c'est crypté !
+	 */
+	public function change_data()
+	{
+		$content = $this->input->post('content');
+		$id = $this->input->post('id');
+		$user = $this->input->post('user');
+		$key = $this->input->post('key');
+		
+		if (strlen($content) == 0 || strlen($id) == 0/* || strlen($user) == 0 || strlen($key) == 0*/)
+		{
+			die("Missing parameter");
+		}
+		
+		if (is_numeric($id) == FALSE)
+		{
+			die("Wrong id");
+		}
+		
+		$this->load->model('request_model');
+		
+		//if ($this->request_model->valid_credentials($user, $key) === TRUE)
+		{
+			$this->request_model->change_item($id, $content);
+		}
+		/*else
+		{
+			die("Invalid credentials");
+		}*/
+	}
+
+}
+
+/* EOF */
---
+++ application/models/request_model.php
@@ -0,0 +1,55 @@
+<?php
+class Request_model extends CI_Model {			
+    public function __construct()
+    {
+        parent::__construct();
+    }
+	
+	/*
+	 * Permet de checker un couple user/mdp
+	 * Ce n'est pas un vrai mdp en fait, mais un mot crypté avec la clef de l'user
+	 */
+	public function valid_credentials($username, $key)
+	{
+		$this->load->helper('security');
+	
+	    // requête préparée, beaucoup plus sécurisé et rapide
+	    $q = "SELECT * FROM key_user WHERE usr_user = ?";
+	    $q = $this->db->query($q, array($username));
+		$r = $q->row_array(); // on récupère le 1er résultat
+	
+	    if ($r['usr_password'] == $key)
+	    {			
+	    	return TRUE;
+	    } else {
+	    	return FALSE;
+		}
+	}
+
+	/*
+	 * CRUD
+	 */
+	public function add_section($title)
+	{
+		$this->db->insert('key_data', array('key_title' => $title)); 
+		
+		// on récupère l'id de l'enregistrement que l'on vien tde faire
+		return mysql_insert_id();
+	}
+	public function get_blops()
+	{
+		$this->db->select('key_id AS id, key_title AS title, key_blop AS blop')->from('key_data');
+		$query = $this->db->get();
+		
+		return $query->result_array(); // le 1er trouvé
+	}
+	public function change_item($id, $data)
+	{
+		$this->db->where('key_id', $id);
+		$this->db->update('key_data', array('key_blop' => $data));
+	}
+
+
+}
+
+/* EOF */
