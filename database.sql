f793bc525d08630f554052b86c6e3dc19a549b0a
Romaric Drigon <romaric.drigon@gmail.com>
2011-09-27 17:08:50 +0200

    Added a database dump
    So you can set it up quickly

---
+++ database.sql
@@ -0,0 +1,48 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9.2
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Mar 27 Septembre 2011 à 17:07
+-- Version du serveur: 5.5.9
+-- Version de PHP: 5.3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `keypass_ez327`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `key_data`
+--
+
+CREATE TABLE `key_data` (
+  `key_id` int(11) NOT NULL AUTO_INCREMENT,
+  `key_title` text NOT NULL,
+  `key_blop` text NOT NULL,
+  PRIMARY KEY (`key_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `key_user`
+--
+
+CREATE TABLE `key_user` (
+  `usr_user` varchar(255) NOT NULL,
+  `usr_password` text NOT NULL,
+  `usr_id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`usr_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
