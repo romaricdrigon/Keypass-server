556f12d9d058db5192a41f00c0986dae914c787e
Romaric Drigon <romaric.drigon@gmail.com>
2011-09-29 16:23:51 +0200

    Updated sql

--- database.sql
+++ database.sql
@@ -3,18 +3,15 @@
 -- http://www.phpmyadmin.net
 --
 -- Serveur: localhost
--- Généré le : Mar 27 Septembre 2011 à 17:07
+-- Généré le : Jeu 29 Septembre 2011 à 16:22
 -- Version du serveur: 5.5.9
 -- Version de PHP: 5.3.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
+--
+-- Base de données
+--
 
 -- --------------------------------------------------------
 
@@ -24,10 +21,16 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 CREATE TABLE `key_data` (
   `key_id` int(11) NOT NULL AUTO_INCREMENT,
+  `key_usr_id` int(11) NOT NULL,
   `key_title` text NOT NULL,
   `key_blop` text NOT NULL,
-  PRIMARY KEY (`key_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  PRIMARY KEY (`key_id`),
+  KEY `key_usr_id` (`key_usr_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- Contenu de la table `key_data`
+--
 
 
 -- --------------------------------------------------------
@@ -41,6 +44,20 @@ CREATE TABLE `key_user` (
   `usr_password` text NOT NULL,
   `usr_id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`usr_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
-INSERT INTO `key_user` VALUES('admin', '"N8Hv0jK9Q5InD0jP+YkHo3LD60E="', 2);
+--
+-- Contenu de la table `key_user`
+--
+
+INSERT INTO `key_user` VALUES('admin', 'N8Hv0jK9Q5InD0jP+YkHo3LD60E=', 1);
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `key_data`
+--
+ALTER TABLE `key_data`
+  ADD CONSTRAINT `key_data_ibfk_1` FOREIGN KEY (`key_usr_id`) REFERENCES `key_user` (`usr_id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
